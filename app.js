7058ec925b90ceebeddc85f04f575e98f368c38a
Novicer18 <sirajudin.seid@astu.edu.et>
2026-01-14 02:17:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv"; 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+// Health check route
+app.get("/", (_req, res) => {
+	res.send("Subscription tracker API is running.");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
+
